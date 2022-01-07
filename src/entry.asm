31ca6a8e9a45831d1591be8de7af5f0a41d43b3b
Junji Wei <weijunji@bytedance.com>
2022-01-07 16:11:55 +0800

    boot: relocate in entry.asm
    Signed-off-by: Junji Wei <weijunji@bytedance.com>

--- src/entry.asm
+++ src/entry.asm
@@ -1,38 +1,35 @@
     .section .text.entry
     .globl _start
 _start:
-    lui     t0, %hi(boot_page_table)
-    # t1 := 0xffffffff40000000 即虚实映射偏移量
-    li      t1, 0xffffffffc0000000 - 0x80000000
-    # t0 减去虚实映射偏移量 0xffffffff40000000，变为三级页表的物理地址
-    sub     t0, t0, t1
-    # t0 >>= 12，变为三级页表的物理页号
+    # physical address of boot page table
+    la t0, boot_page_table
+    # pfn of page table
     srli    t0, t0, 12
-
-    # t1 := 8 << 60，设置 satp 的 MODE 字段为 Sv39
+    # set satp mod to SV39
     li      t1, 8 << 60
-    # 将刚才计算出的预设三级页表物理页号附加到 satp 中
+    # add pfn to satp
     or      t0, t0, t1
-    # 将算出的 t0(即新的MODE|页表基址物理页号) 覆盖到 satp 中
     csrw    satp, t0
-    # 使用 sfence.vma 指令刷新 TLB
     sfence.vma
-    # 从此，我们给内核搭建出了一个完美的虚拟内存空间！
 
+    # relocate to virtual address
+    la t0, relocated
+    li t1, 0xffffffffc0200000 - 0x80200000
+    add t0, t0, t1
+    jr t0
+
+relocated:
     # only support 16 core
     li a2, 16
     bge a0, a2, stop_hart
 
     # load per hart stack
-    lui sp, %hi(boot_stack_top)
-    addi sp, sp, %lo(boot_stack_top)
+    la sp, boot_stack_top
     li a2, 4096
     mul a2, a2, a0
     add sp, sp, a2
 
-    lui t0, %hi(rust_main)
-    addi t0, t0, %lo(rust_main)
-    jr t0
+    tail rust_main
 
 stop_hart:
     # sbi_hart_stop
