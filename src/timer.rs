26e49e13b9466ff77df13d140608119a0717d0aa
Junji Wei <weijunji@bytedance.com>
2022-01-10 16:34:58 +0800

    timer: Add seqlock for TICK
    Signed-off-by: Junji Wei <weijunji@bytedance.com>

--- src/timer.rs
+++ src/timer.rs
@@ -3,20 +3,25 @@ use core::arch::asm;
 
 use crate::arch;
 use crate::sbi::set_timer;
+use crate::sync::SeqLock;
 
-static mut tick: u64 = 0;
+static TICK: SeqLock<u64> = SeqLock::new(0);
 
 const INTERVAL: usize = 100000;
 
 /// set next timer interrupt
 pub fn set_next_timeout() {
     set_timer((arch::read_time() + INTERVAL) as u64);
-    unsafe {
-        tick += 1;
-        if tick % 100 == 0 {
-            println!("tick {}", tick);
-        }
+    let mut tick = TICK.writer_lock();
+    *tick += 1;
+    if *tick % 100 == 0 {
+        println!("tick {}", *tick);
     }
+}
+
+#[allow(dead_code)]
+pub fn read_tick() -> u64 {
+    *TICK.read()
 }
 
 pub fn init() {
